24c184d00ccd53ec480760e13970a22a99200977
Dayana <dayanapinela355@gmail.com>
2023-06-12 19:43:02 -0500

    lección

--- src/app/pages/action-sheet/action-sheet.page.spec.ts
+++ src/app/pages/action-sheet/action-sheet.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { ActionSheetPage } from './action-sheet.page';
 
 describe('ActionSheetPage', () => {
